443f222a760219305c6795a410d0b6648e2ef9e4
EldarMister <marselanarbekovv@gmail.com>
2026-03-06 14:33:35 +0600

    Add category pages and fix logo serving

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(cors({
 }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use('/img', express.static(path.join(__dirname, 'img')));
 
 app.use('/api/orders', ordersRouter);
 app.use('/api/services', servicesRouter);
